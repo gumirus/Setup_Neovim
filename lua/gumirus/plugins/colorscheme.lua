ad9ecec755435e073bf387b514bceff19bcadedf
gumirus <gumirus29@gmail.com>
2024-09-07 20:35:35 +0500

    fix: add nvim-tree.lua file

--- lua/gumirus/plugins/colorscheme.lua
+++ lua/gumirus/plugins/colorscheme.lua
@@ -2,6 +2,45 @@ return {
   "folke/tokyonight.nvim",
   priority = 1000,
   config = function()
+    local bg = "#011628"
+    local bg_dark = "#011423"
+    local bg_light = "#143652"
+    local bg_search = "#0a64ac"
+    local bg_visual = "#275378"
+    local fg = "#cbe0f0"
+    local fg_dark = "#b4doe9"
+    local fg_guter = "#527e97"
+    local border = "#547998"
+
+    require("tokyonight").setup({
+      style = "storm",
+      transparent = false,
+      italic_comments = true,
+      sidebars = { "qf", "vista_kind", "terminal", "packer" },
+      colors = {
+        bg = bg,
+        bg_dark = bg_dark,
+        bg_light = bg_light,
+        bg_search = bg_search,
+        bg_visual = bg_visual,
+        fg = fg,
+        fg_dark = fg_dark,
+        fg_gutter = fg_guter,
+        border = border,
+        comment = "#5c6370",
+        cursor = "#528bff",
+        line = "#202328",
+        selection = "#3e4452",
+        builtin = "#e0af68",
+        keyword = "#c678dd",
+        variable = "#7ec0ee",
+        ["function"] = "#7ec0ee",
+        string = "#98c379",
+        operator = "#56b6c2",
+      },
+    }) -- Закрытие вызова функции здесь
+
     vim.cmd("colorscheme tokyonight")
   end,
 }
+
---
+++ lua/gumirus/plugins/nvim-tree.lua
@@ -0,0 +1,48 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    local nvim_tree = require("nvim-tree")
+
+    -- recommended settings from nvim-tree documentation
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    nvim_tree.setup({
+      view = {
+        width = 35,
+        relativenumber = true,
+      },
+      -- change folder arrow to icon
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            folder = {
+              arrow_closed = "►",
+              arrow_open = "▼",
+            },
+          },
+        },
+      },
+      filters = {
+        custom = {
+          ".DS_Store",
+        },
+        git = {
+          ignore = false,
+        },
+      },
+    })
+
+    -- set keymaps
+    local keymap = vim.keymap -- for convenience
+
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle the file explorer
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse the file explorer
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh the file explorer
+  end
+}
