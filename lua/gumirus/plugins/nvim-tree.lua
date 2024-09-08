b1c203eb5362ed78e5268338287464cc28d426c6
gumirus <gumirus29@gmail.com>
2024-09-08 16:58:05 +0500

    fix: keymaps correction code

--- lua/gumirus/plugins/nvim-tree.lua
+++ lua/gumirus/plugins/nvim-tree.lua
@@ -29,12 +29,17 @@ return {
       filters = {
         custom = { ".DS_Store" },
       },
-    })   
+      git = {
+        ignore = false,
+      },
+    })
 
-    -- Устанавливаем привязки клавиш напрямую через vim.keymap.set
-    vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-    vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
-    vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
-    vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
+    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
   end
 }
