fe118daa531a2246e1be5d99f17f25fdd627b3c5
Ruslan Gumirov <gumirus29@gmail.com>
2024-09-10 12:01:54 +0500

    fixiruem

--- lua/gumirus/core/keymaps.lua
+++ lua/gumirus/core/keymaps.lua
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
@@ -21,28 +21,3 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) -- go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) -- go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) -- open current buffer in new tab
-=======
-vim.g.mapleader = " "
-
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
--- general key keymaps
-keymap("i", "jk", "<ESC>", opts)
-keymap("n", "<leader>nh", ":nohl<CR>", opts)
-keymap("n", "x", '"_x', opts)
-keymap("n", "<leader>+", "<C-a>", opts)
-keymap("n", "<leader>-", "<C-x>", opts)
-
--- window management
-keymap("n", "<leader>sv", "<C-w>v", opts) -- split window vertically
-keymap("n", "<leader>sh", "<C-w>s", opts) -- split window horizontally
-keymap("n", "<leader>se", "<C-w>=", opts) -- make split windows equal width
-keymap("n", "<leader>sx", ":close<CR>", opts) -- close current split
-
-keymap("n", "<leader>to", "<cmd>tabnew<CR>", opts) -- open new tab
-keymap("n", "<leader>tx", "<cmd>tabclose<CR>", opts) -- close current tab
-keymap("n", "<leader>tn", "<cmd>tabn<CR>", opts) -- go to next tab
-keymap("n", "<leader>tp", "<cmd>tabp<CR>", opts) -- go to previous tab
-keymap("n", "<leader>tf", "<cmd>tabnew %<CR>", opts) -- open current buffer in new tabvim.g.mapleader = " "
->>>>>>> 9d9f5b5 (fix: correction keymap)
