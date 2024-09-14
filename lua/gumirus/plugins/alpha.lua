34c70c21d4a3327a9b6fb821ec72fa93a590fd3a
Ruslan Gumirov <gumirus29@gmail.com>
2024-09-14 18:20:02 +0500

    иконки

--- lua/gumirus/plugins/alpha.lua
+++ lua/gumirus/plugins/alpha.lua
@@ -19,12 +19,12 @@ return {
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("e", "📝 > New File", "<cmd>ene<CR>"),
+			dashboard.button("e", "📄 > New File", "<cmd>ene<CR>"),
 			dashboard.button("SPC ee", "📂 > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("SPC ff", "🗂️ > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "🔍 > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC wr", "🔄 > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-			dashboard.button("q", "❌ > Quit NVIM", "<cmd>qa<CR>"),
+			dashboard.button("SPC ff", "🔎🗂 > Find File", "<cmd>Telescope find_files<CR>"),
+			dashboard.button("SPC fs", "📝🔍 > Find Word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("SPC wr", "♻️ > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+			dashboard.button("q", "🚪 > Quit NVIM", "<cmd>qa<CR>"),
 		}
 
 		-- Send config to alpha
